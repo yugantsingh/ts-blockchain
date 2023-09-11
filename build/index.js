af20f51d8cd47d25538ed3f6a03f2ca341760aa1
“Yugant <yugantx@gmail.com>
2023-09-11 19:55:54 +0530

    Including Build

--- build/index.js
+++ build/index.js
@@ -1,1 +1,39 @@
-var hello = function () { return console.log("hi"); };
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto_1 = __importDefault(require("crypto"));
+class Block {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
+        this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto_1.default.createHash("sha256").update(toHash).digest("hex");
+    }
+}
+class Blockchain {
+    constructor() {
+        this.blocks = [];
+    }
+    addBlock(data) {
+        const newblock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newblock);
+    }
+    getPrevHash() {
+        if (this.blocks.length === 0)
+            return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    getBlocks() {
+        return [...this.blocks];
+    }
+}
+const blockchain = new Blockchain();
+blockchain.addBlock("Eth");
+blockchain.addBlock("btc");
+console.log(blockchain.getBlocks());
